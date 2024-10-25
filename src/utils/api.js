c6a6f4d7da3e91fc3022838e2c10059963fba1ae
Carlos170702 <jbsbsbs97@gmail.com>
2024-10-25 00:26:15 -0600

    first commit

---
+++ src/utils/api.js
@@ -0,0 +1,134 @@
+class Api {
+  constructor(baseUrl) {
+    this.baseUrl = baseUrl;
+    this.headers = {
+      Authorization: localStorage.getItem('jwt')
+        ? `Bearer ${localStorage.getItem('jwt')}`
+        : '',
+      'Content-Type': 'application/json',
+    };
+  }
+
+  setToken(token) {
+    this.headers.Authorization = `Bearer ${token}`;
+  }
+
+  getInitialCards() {
+    return fetch(`${this.baseUrl}/cards`, { headers: this.headers })
+      .then((res) => {
+        if (!res.ok) throw new Error(`Server error: ${res.status}`);
+        return res.json();
+      })
+      .catch((error) => console.error('Error', error));
+  }
+
+  getUserInfo() {
+    return fetch(`${this.baseUrl}/users/me`, {
+      headers: this.headers,
+    })
+      .then((res) => {
+        if (!res.ok) throw new Error(`Server error: ${res.status}`);
+        return res.json();
+      })
+      .catch((error) => console.error('Error', error));
+  }
+  updateUser({ name, about }) {
+    return fetch(`${this.baseUrl}/users/me`, {
+      headers: this.headers,
+      method: 'PATCH',
+      body: JSON.stringify({
+        name,
+        about,
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) throw new Error(`Error en servidor: ${res.status}`);
+        return res.json();
+      })
+      .catch((error) => console.error('Error', error));
+  }
+
+  updateAvatar({ avatar }) {
+    return fetch(`${this.baseUrl}/users/me/avatar`, {
+      headers: this.headers,
+      method: 'PATCH',
+      body: JSON.stringify({
+        avatar,
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) throw new Error(`Error en servidor: ${res.status}`);
+        return res.json();
+      })
+      .catch((error) => console.error('Error', error));
+  }
+
+  addCard({ name, link }) {
+    return fetch(`${this.baseUrl}/cards`, {
+      headers: this.headers,
+      method: 'POST',
+      body: JSON.stringify({
+        name,
+        link,
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) throw new Error(`Error en servidor: ${res.status}`);
+        return res.json();
+      })
+      .catch((error) => console.error('Error', error));
+  }
+
+  deleteCard(idCard) {
+    return fetch(`${this.baseUrl}/cards/${idCard}`, {
+      headers: this.headers,
+      method: 'DELETE',
+      body: JSON.stringify({
+        idCard,
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) throw new Error(`Error en servidor: ${res.status}`);
+        return res.json();
+      })
+      .catch((error) => console.error('Error', error));
+  }
+
+  likeCard({ id, isLiked }) {
+    const method = isLiked ? 'PUT' : 'DELETE';
+    return fetch(`${this.baseUrl}/cards/${id}/likes`, {
+      headers: this.headers,
+      method: method,
+      body: JSON.stringify({
+        id,
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) throw new Error(`Error en servidor: ${res.status}`);
+        return res.json();
+      })
+      .catch((error) => console.error('Error', error));
+  }
+
+  unlikeCard(id) {
+    return fetch(`${this.baseUrl}/cards/likes/${id}`, {
+      headers: this.headers,
+      method: 'DELETE',
+      body: JSON.stringify({
+        id,
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) throw new Error(`Error en servidor: ${res.status}`);
+        return res.json();
+      })
+      .catch((error) => console.error('Error', error));
+  }
+}
+
+const api = new Api('http://localhost:3000');
+
+export default api;
+
+/* const api = new Api('https://api.gabriel14.mooo.com');
+ */
---
+++ src/utils/auth.js
@@ -0,0 +1,67 @@
+/* export const BASE_URL = 'https://api.gabriel14.mooo.com'; */
+
+export const BASE_URL = "http://localhost:3000";
+
+export const register = async (email, password) => {
+  try {
+    const response = await fetch(`${BASE_URL}/signup`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.error("Error:", err);
+    throw err;
+  }
+};
+
+export const login = async (email, password) => {
+  try {
+    const response = await fetch(`${BASE_URL}/signin`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+    console.log("🚀 ~ login ~ response:", response);
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.error("Error:", err);
+    throw new Error("Correo o contraseña incorrectos");
+  }
+};
+
+export const getUserToken = async (token) => {
+  try {
+    const response = await fetch(`${BASE_URL}/users/me`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.error("Error:", err);
+    throw err;
+  }
+};
